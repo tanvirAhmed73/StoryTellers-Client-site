454d4a001fd17f8f1db6528a8b6564987fa094c2
tanvir ahmed <tanvir.073.ahmed@gmail.com>
2024-05-25 11:47:01 +0600

    remove jwt

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosSecure = axios.create({
   baseURL: "https://story-tellers-server-site-code.vercel.app",
-  withCredentials: true  
 });
 const UseAxiosSecure = () => {
   return axiosSecure;
